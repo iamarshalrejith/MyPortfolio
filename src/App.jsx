f5a3aa93cd46fbacd83419a04e1c0be4067b057a
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-07-31 21:37:35 +0530

    Add Loading Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css"
 
 const App = () => {
+  const [isLoaded,setIsLoaded] = useState(false) 
   return (
-    <div>App</div>
-  )
-}
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+        {" "}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
 
-export default App
+        </div>
+    </>
+  );
+};
+
+export default App;
